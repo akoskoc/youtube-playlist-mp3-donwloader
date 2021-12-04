6237f1653be7db80c390bbe37e0801851bce00d1
akoskoc <akoskoc@gmail.com>
2021-12-04 11:42:48 +0100

    refactor youtube api

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
-import getOAuth2Client from "./getOAuth2Client";
-import { getYtPlaylistNames, getYtPlaylistItems } from "./ytApi";
+import youtubeApi from "./ytApi";
 import getSelectedPlaylistName from "./getSelectedPlaylistName";
 import settings from "./settings";
 import fs from "fs-extra";
 
 (async function () {
     const saveDir = await settings.getSaveDir();
+    await youtubeApi.init();
 
-    const oauth2Client = await getOAuth2Client();
-    const playlists = await getYtPlaylistNames(oauth2Client);
+    const playlists = await youtubeApi.getYtPlaylistNames();
 
     const selectedPlaylist = await getSelectedPlaylistName(playlists);
 
-    const playlistItems = await getYtPlaylistItems(
-        oauth2Client,
+    const playlistItems = await youtubeApi.getYtPlaylistItems(
         selectedPlaylist.id
     );
+
     fs.ensureDir(saveDir);
 
     fs.writeFileSync(
--- src/ytApi.ts
+++ src/ytApi.ts
@@ -1,99 +1,124 @@
+import getOAuth2Client from "./getOAuth2Client";
 import { OAuth2Client } from "google-auth-library";
-import { google } from "googleapis";
+import { google, youtube_v3 } from "googleapis";
 import { Playlist, PlaylistItem } from "./types";
 
-export function getYtPlaylistNames(
-    oauth2Client: OAuth2Client
-): Promise<Playlist[]> {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const yt = google.youtube("v3");
-            let playlists: Playlist[] = [];
-            let nextPageToken = "";
+const ERR_NOT_AUTHENTICATED = new Error("Authentication error.");
 
-            while (true) {
-                let response = await yt.playlists.list({
-                    auth: oauth2Client,
-                    mine: true,
-                    part: ["id", "snippet"],
-                    maxResults: 50,
-                    pageToken: nextPageToken,
-                });
+class YoutubeApi {
+    private oauth2Client: OAuth2Client | undefined;
+    private yt: youtube_v3.Youtube;
 
-                if (response.data.items) {
-                    for (const playlist of response.data.items) {
-                        if (
-                            playlist.id &&
-                            playlist.snippet &&
-                            playlist.snippet.title
-                        ) {
-                            playlists.push({
-                                id: playlist.id,
-                                title: playlist.snippet.title,
-                            });
+    constructor() {
+        this.yt = google.youtube("v3");
+    }
+
+    init(): Promise<true> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                if (!this.oauth2Client) {
+                    this.oauth2Client = await getOAuth2Client();
+                }
+                resolve(true);
+            } catch (e) {
+                reject(e);
+            }
+        });
+    }
+
+    public getYtPlaylistNames(): Promise<Playlist[]> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                if (!this.oauth2Client) throw ERR_NOT_AUTHENTICATED;
+                let playlists: Playlist[] = [];
+                let nextPageToken = "";
+
+                while (true) {
+                    let response = await this.yt.playlists.list({
+                        auth: this.oauth2Client,
+                        mine: true,
+                        part: ["id", "snippet"],
+                        maxResults: 50,
+                        pageToken: nextPageToken,
+                    });
+
+                    if (response.data.items) {
+                        for (const playlist of response.data.items) {
+                            if (
+                                playlist.id &&
+                                playlist.snippet &&
+                                playlist.snippet.title
+                            ) {
+                                playlists.push({
+                                    id: playlist.id,
+                                    title: playlist.snippet.title,
+                                });
+                            }
                         }
                     }
+
+                    if (!response.data.nextPageToken) {
+                        break;
+                    }
+
+                    nextPageToken = response.data.nextPageToken;
                 }
 
-                if (!response.data.nextPageToken) {
-                    break;
-                }
-
-                nextPageToken = response.data.nextPageToken;
+                resolve(playlists);
+            } catch (e) {
+                reject(e);
             }
+        });
+    }
 
-            resolve(playlists);
-        } catch (e) {
-            reject(e);
-        }
-    });
-}
+    public getYtPlaylistItems(playlistId: string): Promise<PlaylistItem[]> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                if (!this.oauth2Client) throw ERR_NOT_AUTHENTICATED;
 
-export function getYtPlaylistItems(
-    oauth2Client: OAuth2Client,
-    playlistId: string
-): Promise<PlaylistItem[]> {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const yt = google.youtube("v3");
-            let playlistItems: PlaylistItem[] = [];
-            let nextPageToken = "";
+                let playlistItems: PlaylistItem[] = [];
+                let nextPageToken = "";
 
-            while (true) {
-                let response = await yt.playlistItems.list({
-                    auth: oauth2Client,
-                    playlistId: playlistId,
-                    part: ["snippet"],
-                    maxResults: 50,
-                    pageToken: nextPageToken,
-                });
+                while (true) {
+                    let response = await this.yt.playlistItems.list({
+                        auth: this.oauth2Client,
+                        playlistId: playlistId,
+                        part: ["snippet"],
+                        maxResults: 50,
+                        pageToken: nextPageToken,
+                    });
 
-                if (response.data.items) {
-                    for (const playlistItem of response.data.items) {
-                        if (
-                            playlistItem.snippet &&
-                            playlistItem.snippet.title &&
-                            playlistItem.snippet.resourceId &&
-                            playlistItem.snippet.resourceId.videoId
-                        ) {
-                            playlistItems.push({
-                                id: playlistItem.snippet.resourceId.videoId,
-                                title: playlistItem.snippet.title,
-                            });
+                    if (response.data.items) {
+                        for (const playlistItem of response.data.items) {
+                            if (
+                                playlistItem.snippet &&
+                                playlistItem.snippet.title &&
+                                playlistItem.snippet.resourceId &&
+                                playlistItem.snippet.resourceId.videoId
+                            ) {
+                                playlistItems.push({
+                                    id: playlistItem.snippet.resourceId.videoId,
+                                    title: playlistItem.snippet.title,
+                                });
+                            }
                         }
                     }
+
+                    if (!response.data.nextPageToken) {
+                        break;
+                    }
+
+                    nextPageToken = response.data.nextPageToken;
                 }
 
-                if (!response.data.nextPageToken) {
-                    break;
-                }
-
-                nextPageToken = response.data.nextPageToken;
+                resolve(playlistItems);
+            } catch (e) {
+                reject(e);
             }
-
-            resolve(playlistItems);
-        } catch (e) {
-            reject(e);
-        }
-    });
+        });
+    }
 }
+
+const youtubeApi = new YoutubeApi();
+
+export default youtubeApi;
